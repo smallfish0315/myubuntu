5fd02e2fb3a316d6cec855988ff15282da96cbaa
Yongjian Xu <xuyongjiande@gmail.com>
2014-08-13 14:59:34 +0800

    Add software to install

---
+++ install_ppa_chrome.sh
@@ -0,0 +1,3 @@
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
