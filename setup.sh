00c46280ded5ffeaf441cb4dd96fabbea2ae4100
Yongjian Xu <xuyongjiande@gmail.com>
2014-11-18 13:50:16 +0800

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@ THISDIR=`pwd`
 
 mv ~/.bashrc ~/.bashrc.bak
 
-ln $THISDIR/_bashrc ~/
-ln $THISDIR/_testbash.py ~/
-ln $THISDIR/_lxr_3.12.sh ~/
-ln $THISDIR/_gitconfig ~/
-ln $THISDIR/_tmux.conf ~/
-ln $THISDIR/_ssh_config ~/.ssh/config
+ln -f $THISDIR/_bashrc ~/.bashrc
+ln -f $THISDIR/_testbash.py ~/.testbash.py
+ln -f $THISDIR/_gitconfig ~/.gitconfig
+ln -f $THISDIR/_tmux.conf ~/.tmux.conf
+ln -f $THISDIR/_ssh_config ~/.ssh/config
