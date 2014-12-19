2f11bd4f34771189191a8bd046bc7ae636affafb
Yongjian Xu <xuyongjiande@gmail.com>
2014-12-19 12:29:12 +0800

    Some change

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 THISDIR=`pwd`
 
-mv ~/.bashrc ~/.bashrc.bak
-
-ln -f $THISDIR/_bashrc ~/.bashrc
-ln -f $THISDIR/_testbash.py ~/.testbash.py
-ln -f $THISDIR/_gitconfig ~/.gitconfig
-ln -f $THISDIR/_tmux.conf ~/.tmux.conf
-ln -f $THISDIR/_ssh_config ~/.ssh/config
+ln -sf $THISDIR/_bashrc ~/.bashrc
+ln -sf $THISDIR/_testbash.py ~/.testbash.py
+ln -sf $THISDIR/_gitconfig ~/.gitconfig
+ln -sf $THISDIR/_tmux.conf ~/.tmux.conf
+ln -sf $THISDIR/_ssh_config ~/.ssh/config
